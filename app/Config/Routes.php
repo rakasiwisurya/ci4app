89709912365707d95e18dd9f8ee61bb14c3b6d48
rakasiwisurya <rakasiwisurya.nutech@gmail.com>
2022-08-29 17:09:30 +0700

    done learn controller and routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,6 +36,10 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/coba', 'Coba::index');
+$routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2');
+
+$routes->get('/users', 'Admin\Users::index');
 
 /*
  * --------------------------------------------------------------------
